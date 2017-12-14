bbe1ce381014bdccded8491c2bdcf55e020a7f61
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-14 09:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(obj,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
-    obj[key]='value'
+    obj[key]=value
     return obj
 }
 
